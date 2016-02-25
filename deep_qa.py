f9aabe0a25fcb4f85cec0098f3b8f6adfd86e350
bwallace <byron.wallace@gmail.com>
2016-02-25 12:36:50 -0600

    possibly working?

--- deep_qa.py
+++ deep_qa.py
@@ -1,6 +1,8 @@
 import pdb
 import operator
 
+import cPickle 
+
 import numpy as np
 import pandas as pd 
 
@@ -14,7 +16,7 @@ try:
     from cochranenlp.readers.biviewer import PDFBiViewer
 except:
     print("cochrannlp not found!")
-    
+
 import keras
 from keras.layers.embeddings import Embedding
 from keras.layers.core import Dense, Merge, Activation, RepeatVector, TimeDistributedDense
@@ -27,6 +29,9 @@ from keras.layers.recurrent import LSTM, GRU
 
 import gensim 
 from gensim.models import Word2Vec
+
+START_STR = "startstartstart"
+STOP_STR  = "stopstopstop"
 
 '''
 def _get_init_vectors(vectorizer, wv, unknown_words_to_vecs):
@@ -63,15 +68,16 @@ def get_docs_and_intervention_summaries(pico_elem_str="CHAR_INTERVENTIONS"):
 class ISummarizer:
 
     # 100000
-    def __init__(self, pairs, nb_words=10000, hidden_size=200, max_input_size=5000, max_output_size=50):
+    def __init__(self, pairs, nb_words=10000, hidden_size=128, max_input_size=5000, max_output_size=50):
         self.pairs = pairs 
-        self.nb_words = nb_words
+        self.nb_words = nb_words + 2 # number of words; +2 for start and stop tokens!
         self.max_input_size = max_input_size
-        self.max_output_size = max_output_size
+        self.max_output_size = max_output_size + 2 # again +2 for start/stop
 
         self.hidden_size = hidden_size
         print("loading pre-trained word vectors...")
         self.wv = load_trained_w2v_model()
+        # here you want to add start and stop 
         print("OK!")
         self.word_embedding_size = self.wv.vector_size 
 
@@ -88,20 +94,20 @@ class ISummarizer:
     def build_sequences(self):
         self.tokenizer = Tokenizer(nb_words=self.nb_words)
 
-        raw_input_texts = [" ".join(pair[0]) for pair in self.pairs]
-        raw_output_texts = [" ".join(pair[1]) for pair in self.pairs]
+        self.raw_input_texts  = [START_STR + " " + " ".join(pair[0]) + " " + STOP_STR for pair in self.pairs]
+        self.raw_output_texts = [START_STR + " " + " ".join(pair[1]) + " " + STOP_STR for pair in self.pairs]
 
         def _get_max(seqs):
             return max([len(seq) for seq in seqs])
 
-        self.tokenizer.fit_on_texts(raw_input_texts+raw_output_texts)
-        self.input_sequences  = list(self.tokenizer.texts_to_sequences_generator(raw_input_texts))
+        self.tokenizer.fit_on_texts(self.raw_input_texts+self.raw_output_texts)
+        self.input_sequences  = list(self.tokenizer.texts_to_sequences_generator(self.raw_input_texts))
         #self.max_input_len    = _get_max(self.input_sequences)
         #X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
         #X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
         self.input_sequences = list(pad_sequences(self.input_sequences, maxlen=self.max_input_size))
 
-        self.output_sequences = list(self.tokenizer.texts_to_sequences_generator(raw_output_texts))
+        self.output_sequences = list(self.tokenizer.texts_to_sequences_generator(self.raw_output_texts))
         self.output_sequences = list(pad_sequences(self.output_sequences, maxlen=self.max_output_size))
         
     def init_word_vectors(self):
@@ -125,31 +131,23 @@ class ISummarizer:
         self.model = Sequential()
         self.model.add(Embedding(self.nb_words, self.word_embedding_size, weights=[self.init_vectors]))
         ### 
-        # I think we only want the output dim here!
-        #model.add(GRU(self.word_embedding_size, self.hidden_size))
+        # run embeddings through a Gated Recurrent Unit
         self.model.add(GRU(self.hidden_size))
-        # pretty sure these do, as well
-        #model.add(Dense(self.hidden_size, self.hidden_size))
         self.model.add(Dense(self.hidden_size))
         self.model.add(Activation('relu'))
         self.model.add(RepeatVector(self.max_output_size))
-        #model.add(GRU(self.hidden_size, self.hidden_size, return_sequences=True))
         self.model.add(GRU(self.hidden_size, return_sequences=True))
-        #model.add(TimeDistributedDense(self.hidden_size, self.nb_words, activation="softmax"))
         self.model.add(TimeDistributedDense(self.nb_words, activation="softmax"))
         # does cross entropy make sense here?
-        #model.compile(loss='binary_crossentropy', optimizer='adam', class_mode="binary")
-        self.model.compile(loss='mse', optimizer='adam')
+        self.model.compile(loss='binary_crossentropy', optimizer='adam', class_mode="binary")
         return self.model 
 
     def X_y(self):
-        #n = len(self.input_sequences)
-        n = 1000 # testing!
-        self.X = np.zeros((n, self.max_input_size, self.nb_words), dtype=np.bool)
-        self.Y = np.zeros((n, self.max_output_size, self.nb_words), dtype=np.bool)
+        self.X = np.array(self.input_sequences) # np.zeros((n, self.max_input_size, self.nb_words), dtype=np.bool)
+        self.Y = np.zeros((len(self.output_sequences), self.max_output_size, self.nb_words), dtype=np.bool)
         for i in range(n):
-            for j, token_idx in enumerate(self.input_sequences[i]):
-                self.X[i, j, token_idx] = 1
+            #for j, token_idx in enumerate(self.input_sequences[i]):
+            #    self.X[i, j, token_idx] = 1
 
             for j, token_idx in enumerate(self.output_sequences[i]):
                 self.Y[i, j, token_idx] = 1
@@ -157,7 +155,87 @@ class ISummarizer:
         print "X shape: %s; Y shape: %s" % (self.X.shape, self.Y.shape)
 
     def train(self):
-        pass
+        # @TODO revisit; batchsize, etc
+        print "fitting model..."
+        self.model.fit(self.X, self.Y)
+
+def all_systems_go():
+    from keras.callbacks import ModelCheckpoint
+    pairs = cPickle.load(open("pairs.pickle"))
+    
+    IS = ISummarizer(pairs)
+    model = IS.build_model()
+    IS.X_y()
+
+    n_train = 12751
+
+    X_train, Y_train = IS.X[:n_train], IS.Y[:n_train]
+    X_test, Y_test   = IS.X[n_train:], IS.y[n_train:]
+
+    checkpointer = ModelCheckpoint(filepath="weights.hdf5", verbose=1, save_best_only=True)
+    model.fit(X_train, Y_train, batch_size=128, nb_epoch=25, verbose=0, 
+                validation_data=(X_test, Y_test), callbacks=[checkpointer])
+
+
+'''
+from keras.callbacks import ModelCheckpoint
+
+model = Sequential()
+model.add(Dense(10, input_dim=784, init='uniform'))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+#saves the model weights after each epoch if the validation loss decreased
+checkpointer = ModelCheckpoint(filepath="/tmp/weights.hdf5", verbose=1, save_best_only=True)
+model.fit(X_train, Y_train, batch_size=128, nb_epoch=20, verbose=0, validation_data=(X_test, Y_test), callbacks=[checkpointer])
+'''
+
+def toy():
+    nb_w = 10
+    embedding_size = 5
+
+    #embeddings = [np.random.random(5) for _ in range(10)]
+
+    X = [[9, 8, 3], [4, 5, 1], [3, 6, 3], [6,2,1]]
+    y = [[1, 2], [3,1], [1,6], [5,2]]
+
+
+    X_arr  = np.array(X) #np.zeros((len(X), len(X[0]), 10)) # n sample; time steps; vocab size
+    y_tens = np.zeros((len(y), len(y[0]), 10)) # n sample; time steps; vocab size
+
+    for i in range(len(X)):
+        #for j, t_idx in enumerate(X[i]):
+        #    X_tens[i, j, t_idx] = 1
+
+        for j, t_idx in enumerate(y[i]):
+            y_tens[i, j, t_idx] = 1
+        
+
+    model = Sequential()
+    model.add(Embedding(10, embedding_size))
+
+    max_output_size = 2
+    hidden_size = 8
+
+    ### 
+    # I think we only want the output dim here!
+    #model.add(GRU(self.word_embedding_size, self.hidden_size))
+    model.add(GRU(hidden_size))
+    model.add(Dense(hidden_size))
+
+    model.add(Activation('relu'))
+
+    model.add(RepeatVector(max_output_size))
+    model.add(GRU(hidden_size, return_sequences=True))
+    model.add(TimeDistributedDense(nb_w, activation="softmax"))
+
+    model.compile(loss='mse', optimizer='adam')
+    
+    model.fit(X_arr, y_tens)
+
+
+if __name__ == "__main__":
+    all_systems_go()
 
 '''
 module load cuda
